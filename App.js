df79097f8924f529a1db84fa277981b6f3e707c1
Sami-ith <56152756+Sami-ith@users.noreply.github.com>
2020-12-18 09:29:40 +0100

    First Commit

--- App.js
+++ App.js
@@ -1,21 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import {View, Image } from "react-native";
+import Card from './Card.js';
+import HomeScreen from './HomeScreen';
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
-export default function App() {
+function DetailsScreen({ route, navigation }) {
+  const { country} = route.params;
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Image
+        source={{
+          uri:
+            "https://flagcdn.com/h240/" +
+            country.alpha2Code.toLowerCase() +
+            ".png",
+        }}
+        style={{ width: 100, height: 100 }}
+      />
+      <Card item={JSON.stringify(country.name)} title="Name" />
+      <Card item={JSON.stringify(country.region)} title="Region" />
+      <Card item={JSON.stringify(country.area)} title="Area" />
+      <Card item={JSON.stringify(country.population)} title="Population" />
+      <Card item={JSON.stringify(country.capital)} title="Capital" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Stack = createStackNavigator();
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Home"
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: "#16a596",
+          },
+          headerTintColor: "#fff",
+          headerTitleStyle: {
+            fontWeight: "bold",
+          },
+        }}
+      >
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+            title: "Home",
+          }}
+        />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
+ 
---
+++ HomeScreen.js
@@ -0,0 +1,59 @@
+import React from "react";
+import { useState } from "react";
+import { StyleSheet, View, TextInput, Alert } from "react-native";
+
+export default function HomeScreen({ navigation }) {
+ 
+  const [name, setName] = useState("");
+  var country;
+  
+ 
+  
+  return (
+    <View
+      style={{
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        fontWeight: "bold",
+      }}
+    >
+      <TextInput
+        style={styles.searchBox}
+        placeholder="🔍  Type country name  "
+        onChangeText={(text) => setName(text)}
+        onSubmitEditing={() => {
+          fetch("https://restcountries.eu/rest/v2/name/" + name)
+            .then((response) => response.json())
+            .then((result) => {
+              country = result[0];
+              if (!country) {
+                alert("Not found try again");
+                setName("");
+              } else {
+                navigation.navigate("Details", {
+                  country: country,
+                });
+                setName("");
+              }
+            });
+        }}
+        value={name}
+      />
+      
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  searchBox: {
+    borderRadius:50,
+    backgroundColor: "#fff",
+    shadowOffset: { width: 1, height: 1 },
+    shadowColor: "#333",
+    shadowOpacity: 0.3,
+    shadowRadius: 2,
+    width: "80%",
+    height: "10%",
+    textAlign: 'center',
+  },
+});
